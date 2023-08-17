3cc6c566495fe4c5da37a5dbb20215ad83466f2f
shwetasharma1302 <126080003+shwetasharma1302@users.noreply.github.com>
2023-08-17 16:03:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here
+let bandNames = ['The Rolling Stones', 'Led Zeppelin', 'Aerosmith', 'The Beatles', 'Red Hot Chili Peppers', 'Nirvana'];
+
+const articles = ['a', 'an', 'the'];
+
+// Function to remove articles from a band name
+function removeArticles(name) {
+    for (let article of articles) {
+        if (name.toLowerCase().startsWith(article + ' ')) {
+            return name.substring(article.length + 1); // +1 to remove the space after the article
+        }
+    }
+    return name;
+}
+
+// Sort the band names without articles
+bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Create the HTML list
+let ul = document.createElement('ul');
+ul.setAttribute('id', 'band');
+
+for (let name of bandNames) {
+    let li = document.createElement('li');
+    li.textContent = name;
+    ul.appendChild(li);
+}
+
+// Append the list to the body of the document
+document.body.appendChild(ul);
 
